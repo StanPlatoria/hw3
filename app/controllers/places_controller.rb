287d64f6cf3bc62dab515a1ee2e446d48cc28377
Stan <120746167+StanPlatoria@users.noreply.github.com>
2026-02-21 18:56:04 +0000

    ....

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,11 @@
+class PlacesController < ApplicationController
+def index
+    @places = Place.all
+  end
+  def show
+  @place = Place.find_by({ "id" => params["id"] })
+  @entries = Entry.where({ "place_id" => @place["id"] })
+end
+def new
+  end
+end
